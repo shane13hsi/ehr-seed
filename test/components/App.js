22e42e44e7a43ea51dfcccb913b7526961d80ae1
Shane Hsi <shane13hsi@gmail.com>
2015-06-28 16:42:23 +0800

    Add a test to show how to use shalow rendering

--- test/components/App.js
+++ test/components/App.js
@@ -1,7 +1,17 @@
 import App from 'scripts/components/App';
+import React from 'react';
+import { expect } from 'chai';
+var TestUtils = React.addons.TestUtils;
 
-describe('App', function () {
-    it('should create a new instance of WebpackApp', function () {
+describe('App', function() {
+    it('should create a new instance of WebpackApp', function() {
         App.should.be.ok;
     });
+
+    it('should emit the HTML output', function() {
+        var shallowRenderer = TestUtils.createRenderer();
+        shallowRenderer.render(<App />);
+        var component = shallowRenderer.getRenderOutput();
+        expect(component.type).to.equal('div');
+    })
 });
